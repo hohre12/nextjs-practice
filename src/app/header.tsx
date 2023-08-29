4cf43518dcf4ee4d8a465b84069d7a0a5107e26c
BaeJaeWon <hohre12@gmail.com>
2023-08-29 15:26:22 +0900

    ADD] 헤더 작업중

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,30 +1,44 @@
+'use client';
 import Link from "next/link"
 import Image from 'next/image'
+import styles from './header.module.scss'
 
 type Props = {};
 function Header({}: Props) {
+    function goPro(e: React.MouseEvent) {
+        console.log('goPro', e)
+    }
     return (
-        <header>
-            <Link href="/">
-                <Image src="svg/main/logo.svg" alt="logo" width={100} height={24} priority></Image>
-            </Link>
-            <nav>
-                <Link href="/service">
-                    주요기능
+        <header className={styles.header}>
+            <div className={`${styles.wrap} ${styles.flex}`}>
+                <Link href="/">
+                    <Image src="svg/main/logo.svg" alt="logo" width={205} height={24} priority></Image>
                 </Link>
-                <Link href="/price">
-                    요금안내
-                </Link>
-                <Link href="/voucher">
-                    비대면바우처
-                </Link>
-                <Link href="/survey">
-                    전수조사
-                </Link>
-                <Link href="/blog">
-                    블로그
-                </Link>
-            </nav>
+                <nav className={styles.nav}>
+                    <ul className={styles.menu}>
+                        <Link href="/service">
+                            주요기능
+                        </Link>
+                        <Link href="/price">
+                            요금안내
+                        </Link>
+                        <Link href="/voucher">
+                            비대면바우처
+                        </Link>
+                        <Link href="/survey">
+                            전수조사
+                        </Link>
+                        <Link href="/blog">
+                            블로그
+                        </Link>
+                    </ul>
+                    <div className={styles.buttonWrap}>
+                        <button className={styles.btnPrimary} onClick={goPro}>로그인</button>
+                        <button className={styles.btnYellow}>무료체험</button>
+                        <button className={styles.btnYellow}>문의하기</button>
+                    </div>
+                </nav>
+            </div>
         </header>
     )
 }
--- src/app/service/page.tsx
+++ src/app/service/page.tsx
@@ -1,12 +1,21 @@
 import React from "react";
+import styles from './service.module.scss'
 
 type Props = {};
 
 function Service({}: Props) {
   return (
-    <div className="border-4 border-yellow-500 text-2xl text-yellow-400 p-2">
-      서비스 페이지 입니다~
-    </div>
+    // <div className="border-4 border-yellow-500 text-2xl text-yellow-400 p-2">
+    // </div>
+      <div>
+        <div className={styles.intro}>
+          <div className={styles.wrap}>
+            <h1>랩매니저 PRO로<br />연구실 환경이 확 바뀝니다!</h1>
+            <h6>물품관리부터 구매까지 한번에 가능한<br />랩매니저 PRO의 주요 기능을 살펴보세요.</h6>
+          </div>
+        </div>
+      </div>
+    
   );
 }
 
